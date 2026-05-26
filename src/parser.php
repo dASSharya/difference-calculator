ee4ac9312dd5d151635e0c1facef3caba3f5c779
dASSharya <dasharya299@me.com>
2026-05-26 17:37:20 +0300

    step-3

---
+++ src/parser.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+<?php
+
+function parse($file) {
+  $data = json_decode($file);
+  $parsedData = [];
+
+  foreach($data as $key => $value) {
+    $parsedData[$key] = $value;
+  }
+  
+  return $parsedData;
+}
